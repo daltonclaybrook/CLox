fb0e5b6661167d8616491001c3a01a00dc4874e3
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-08-13 09:58:38 -0400

    Initial Commit

---
+++ CLox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLox
+//
+//  Created by Dalton Claybrook on 8/13/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
