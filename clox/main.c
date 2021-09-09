5ef230a1f2cec5d7584da75492faec51bc07b928
Dalton Claybrook <dalton.claybrook@onepeloton.com>
2021-09-08 21:42:17 -0400

    Initial Commit

---
+++ clox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  clox
+//
+//  Created by Dalton Claybrook on 9/8/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
